8a6d6f8b2ef8b5892756fa5d9108d6023ea99d43
Alexander Sheremet <13az111o@gmail.com>
2020-07-09 15:39:45 +0300

    homework1_task1_first_commit

---
+++ homeWork_01/ex1_ObjectsAroundUs/sheremet/src/main/java/by/jrr/sheremet/task1/objects/Stove.java
@@ -0,0 +1,13 @@
+package by.jrr.sheremet.task1.objects;
+
+public class Stove {
+    public Stove() {
+        System.out.println("Плита  \n" +
+                "\n" +
+                "Состояние:\n" +
+                "- вкл/выкл\n" +
+                "- температура\n" +
+                "Поведение:\n" +
+                "- вкл/выкл\n");
+    }
+}
---
+++ homeWork_01/ex1_ObjectsAroundUs/sheremet/src/main/java/by/jrr/sheremet/task1/objects/WaterTap.java
@@ -0,0 +1,14 @@
+package by.jrr.sheremet.task1.objects;
+
+public class WaterTap {
+    public WaterTap() {
+        System.out.println("Кран с водой\n" +
+                "\n" +
+                "Состояние:\n" +
+                "-  открыт/закрыт\n" +
+                "- смеситель хол/гор\n" +
+                "Поведение:\n" +
+                "- открыть/закрыть (при открытии течёт вода) \n" +
+                "- переключить воду хол/гор");
+    }
+}
