df9c610419c935daf361d8b0d65571f525a7c96c
twikssi <limpbiski15@gmail.com>
2020-07-10 15:40:11 +0300

    homework 1 task 1 create class 'sunGlasses' correct class 'sunGlasses'

--- homeWork_01/ex1_ObjectsAroundUs/AndryBorisevich/src/main/java/AndryBorisevich/SunGlasses.java
+++ homeWork_01/ex1_ObjectsAroundUs/AndryBorisevich/src/main/java/AndryBorisevich/SunGlasses.java
@@ -4,6 +4,6 @@ public class SunGlasses {
     private boolean sun = true;
 
     public void putSunGlassesOn (){
-        System.out.println("you are wearing sun glasses");
+        System.out.println("you are wearing the sun glasses");
     }
 }
