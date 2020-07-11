eea67e32d7daae42fb7401b1fe567d0c386c2c91
LytkinaOlga <65194987+LytkinaOlga@users.noreply.github.com>
2020-07-11 15:54:23 +0300

    Create second homework task 3.

---
+++ homeWork_02/ex3_ArithmeticOperations/lytkina/src/main/java/by/bntu/fitr/poisit/lytkina/MathOperationsDemo.java
@@ -0,0 +1,33 @@
+package by.bntu.fitr.poisit.lytkina;
+
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 20;
+        System.out.println("the division is " + division(firstNumber, secondNumber));
+        System.out.println("the sum is " + addition(firstNumber, secondNumber));
+        System.out.println("the multiplication is " + multiplication(firstNumber, secondNumber));
+        System.out.println("the subtraction is " + subtraction(firstNumber,secondNumber));
+
+    }
+    public static int addition(int firstNumber, int secondNumber){
+        int result = firstNumber + secondNumber;
+        return result;
+    }
+    public static int subtraction(int firstNumber, int secondNumber){
+        int result = firstNumber - secondNumber;
+        return result;
+    }
+    public static double division (int firstNumber, int secondNumber){
+        double result;
+        if (secondNumber!=0) {
+            result =  (double)firstNumber / (double)secondNumber;
+        }
+        else result = 0;
+        return  result;
+    }
+    public static int multiplication (int firstNumber, int secondNumber){
+        int result = firstNumber * secondNumber;
+        return result;
+    }
+}
