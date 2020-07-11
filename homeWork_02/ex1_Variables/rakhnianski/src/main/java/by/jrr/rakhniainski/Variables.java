9153104fa6e3e5f2cf73d9d3ef6a3c261cfa0177
roma-rakhnianski <cptbaklajan@gmail.com>
2020-07-11 20:28:06 +0300

    Adding Hw2 Task 1

---
+++ homeWork_02/ex1_Variables/rakhnianski/src/main/java/by/jrr/rakhniainski/Variables.java
@@ -0,0 +1,25 @@
+package by.jrr.rakhniainski;
+
+public class Variables {
+
+        int shipNumber;
+        int intCounter;
+        int firstNumber;
+        String answer;
+        int randomNumber;
+        String carColour;
+        float carSpeed;
+        int userNumber;
+        int maxNumber;
+        int minNumber;
+        int leftBorderNumber;
+        int rightBorderNumber;
+        int userRiddledNumber;
+        String answerVariant;
+        float bankAccount;
+        float creditCard;
+        int bicycleWheels;
+        int ticketCost;
+
+    }
+
