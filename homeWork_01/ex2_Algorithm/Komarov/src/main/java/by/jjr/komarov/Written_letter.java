ab63f76e4b8c0e367555333fe2f363ca99c64009
igor <ufolog1368@gmail.com>
2020-07-11 18:25:15 +0300

    Create first homework

---
+++ homeWork_01/ex2_Algorithm/Komarov/src/main/java/by/jjr/komarov/Written_letter.java
@@ -0,0 +1,11 @@
+package by.jjr.komarov;
+
+public class Written_letter {
+    public static void main(String[] args) {
+        System.out.println("Search pen");
+        System.out.println("take a piece oh paper");
+        System.out.println("think over the topic");
+        System.out.println("Written text");
+        System.out.println("put aletter in an envelope");
+    }
+}
