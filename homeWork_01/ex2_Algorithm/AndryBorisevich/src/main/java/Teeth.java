c5dd2e6bc7e9172c4a34786597c5d0feee0dc650
twikssi <limpbiski15@gmail.com>
2020-07-12 17:53:01 +0300

    Create first homework task 2

---
+++ homeWork_01/ex2_Algorithm/AndryBorisevich/src/main/java/Teeth.java
@@ -0,0 +1,25 @@
+public class Teeth {
+    public void runTeeth(){
+        getUp();
+        takeToothbrush();
+        cleanTeeth();
+        sipWater();
+    }
+
+    public void getUp (){
+        System.out.println("Get up");
+        System.out.println("Go to the bathroom");
+    }
+    public void takeToothbrush (){
+        System.out.println("Take teethbrush");
+        System.out.println("Take some toothpaste");
+    }
+
+    public void cleanTeeth(){
+        System.out.println("Clean my teeth");
+    }
+
+    public void sipWater (){
+        System.out.println("Rinse my mouth");
+    }
+}
