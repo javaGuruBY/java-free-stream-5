4d082c06523270e67c7897ed732371767cd65764
igor <ufolog1368@gmail.com>
2020-07-14 21:17:31 +0300

    check please my HW1

---
+++ homeWork_01/ex1_ObjectsAroundUs/komarov/src/main/java/by/jrr/komarov/Tree.java
@@ -0,0 +1,17 @@
+package by.jrr.komarov;
+
+public class Tree {
+    public static void main(String[] args) {
+        System.out.println("Объект - Дерево");
+        System.out.println("Состояние:");
+        System.out.println("-Твердое");
+        System.out.println("-Живое");
+        System.out.println("Поведение:");
+        System.out.println("-Шумит листвой");
+        System.out.println("-Качаетсяна ветру");
+
+
+
+
+    }
+}
