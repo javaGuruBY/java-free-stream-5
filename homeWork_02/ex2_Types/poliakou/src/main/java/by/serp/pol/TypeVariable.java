4ecb4c7461842386d2f79a317cd69fd8832cb1d8
Siarhei_Paliakou <serpolak@gmail.com>
2020-07-15 01:09:05 +0300

    SergeiPoliakou_Hw2_Task2

---
+++ homeWork_02/ex2_Types/poliakou/src/main/java/by/serp/pol/TypeVariable.java
@@ -0,0 +1,27 @@
+package by.serp.pol;
+
+public class TypeVariable {
+    /*
+    Определить тип переменных:
+    Количество кораблей в порту - countShipsInPort (byte, int, short)
+    Счетчик целых чисел - countIntNumber (int, short, long)
+    Первое число - firstNumber (int, double)
+    Ответ на вопрос - answerQuestion (String)
+    Случайное число - randomNumber (int, long, double)
+    Генератор случайных чисел - randomGenerator (int, double)
+    Цвет автомобиля - carColor (String)
+    Скорость движения автомобиля - speed (int)
+    Введеное число пользователем - numberEnter (int, double)
+    Наибольшее число - maxNumber (int, long, double)
+    Наименьшее число - minNumber (int, double)
+    Левая граница диапазона чисел - leftBorder (int, double)
+    Правая граница диапазона чисел - rightBorder (int, double)
+    Загаданное пользователем число - guessNumber (int, double)
+    Вариант ответа - possibleAnswer - (String)
+    Банковский счет - bankAccount - (float, double)
+    Банковский баланс - bankBalance -(float, double)
+    Кредитная карта - creditCart - (float, double)
+    Количество колес у велосипеда - numberWheelsBicycle (byte, int, short)
+    Стоимость билета - ticketPrice (int, double, float)
+    */
+}
