99c0aa0de8cc53a8cb1ad485f3352e3018619d9c
NikMassV <56344046+NikMassV@users.noreply.github.com>
2020-07-16 22:17:26 +0300

    Create fields with types

---
+++ src/main/java/by/jrr/maistrenko/Fields.java
@@ -0,0 +1,27 @@
+package by.jrr.maistrenko;
+
+import java.util.Calendar;
+
+public class Fields {
+    int numberShipsAtPort;
+    int counter;
+    int firstInteger;
+    int randomInteger;
+    double randomDouble;
+    double speedCar;
+    int enteredInteger;
+    int max;
+    double min;
+    int leftBorderOfIntegers;
+    int rightBorderOfIntegers;
+    int userDefinedInteger;
+    double cashBalance;
+    int numberBicyclesWheels;
+    boolean isPersonHungry;
+    char latinAlphabetSymbol;
+    boolean isNumberEven;
+    Calendar currentYear;
+    char spaceCharacter;
+    double ticketPrice;
+}
+
