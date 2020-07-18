9e05a56813f0c5e6a941520b6838da845976b577
Siarhei_Paliakou <serpolak@gmail.com>
2020-07-18 08:59:36 +0300

    I did homework2_ex4

--- homeWork_02/ex4_RandomNumberGenerator/poliakou/src/main/java/by/serg/pol/RandomGeneratorDemo.java
+++ homeWork_02/ex4_RandomNumberGenerator/poliakou/src/main/java/by/serg/pol/RandomGeneratorDemo.java
@@ -1,7 +1,42 @@
 package by.serg.pol;
 
+import java.util.Random;
+
 public class RandomGeneratorDemo {
+
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Random randomGenerator;
+        randomGenerator = new Random();
+
+        int firstRandomNumber = randomGenerator.nextInt();
+        int nextRandomNumber = randomGenerator.nextInt();
+        int lastRandomNumber = randomGenerator.nextInt(100);
+
+        System.out.println(firstRandomNumber);
+        System.out.println(nextRandomNumber);
+        System.out.println(lastRandomNumber);
+
+        int summaRandonNumber = firstRandomNumber + nextRandomNumber + lastRandomNumber;
+
+        System.out.println("summaRandonNumber = " + summaRandonNumber);
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
