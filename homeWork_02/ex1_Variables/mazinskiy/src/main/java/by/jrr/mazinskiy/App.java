2ab0bdec00f9d558c22a9af608654190e333c184
Kirill Mazinski <chikkahikka@mail.ru>
2020-07-19 15:05:59 +0300

    My hw2task1 has been reworked

--- homeWork_02/ex1_Variables/mazinskiy/src/main/java/by/jrr/mazinskiy/App.java
+++ homeWork_02/ex1_Variables/mazinskiy/src/main/java/by/jrr/mazinskiy/App.java
@@ -2,12 +2,12 @@ package by.jrr.mazinskiy;
 
 public class App {
     public static void main(String[] args) {
-       HomeAnimal a = new Cat("krisa",32, "blue");
-       HomeAnimal b = new Dog("Boris",32, "red");
-       a.eat();
-       a.voice();
-       b.voice();
-       b.sleep();
+       HomeAnimal animalOne = new Cat("krisa",32, "blue");
+       HomeAnimal animalTwo = new Dog("Boris",32, "red");
+        animalOne.eat();
+        animalOne.voice();
+        animalTwo.voice();
+        animalTwo.sleep();
 
 
     }
