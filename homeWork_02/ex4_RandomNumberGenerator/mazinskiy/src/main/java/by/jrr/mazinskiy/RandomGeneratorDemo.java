f15660c8c2dc8a1926bcf73c2c9ae81886ce3f0e
Kirill Mazinski <chikkahikka@mail.ru>
2020-07-19 17:17:43 +0300

    hw2Task3 done. Check pls!

---
+++ homeWork_02/ex4_RandomNumberGenerator/mazinskiy/src/main/java/by/jrr/mazinskiy/RandomGeneratorDemo.java
@@ -0,0 +1,11 @@
+package by.jrr.mazinskiy;
+
+public class RandomGeneratorDemo {
+    public static void main(String[] args) {
+        double a = Math.random();
+        double b = Math.random();
+        double c = Math.random();
+        System.out.println(a + b + c);
+
+    }
+}
