c85d75ef10bacf01ca9e862967ba22411c98f7fa
rus-sto <rus-sto@tut.by>
2020-07-21 23:19:28 +0300

    I did HW2E1

---
+++ homeWork_02/ex1_Variables/ruslan_sto/src/main/java/by/jrr/ruslan_sto/Variables.java
@@ -0,0 +1,28 @@
+package by.jrr.ruslan_sto;
+
+public class Variables {
+    int shipsInThePort; //Количество кораблей в порту
+    public static void main(String[] args) {
+
+        //1.	Количество кораблей в порту  - shipsInThePort
+//2.	Счетчик целых чисел  - complimentNumber
+//3.	Первое число	- firstNumber
+//4.	Ответ на вопрос	- answerTheQuestion
+//5.	Случайное число	- randomNumber
+//6.	Генератор случайных чисел	-randomNumberGenerator
+//7.	Цвет автомобиля	- autoColor
+//8.	Скорость движения автомобиля	- autoSpeed
+//9.	Введеное число пользователем 	- numberEnteredByUser
+//10.	Наибольшее число	- maxNumber
+//11.	Наименьшее число	- minNumber
+//12.	Левая граница диапазона чисел	-left_NumbersBorder
+//13.	Правая граница диапазона чисел	- right_NumbersBorder
+//14.	Загаданное пользователем число	- userGuessedNumber
+//15.	Вариант ответа 	- possibleAnswer
+//16.	Банковский счет	- bankAccount
+//17.	Банковский баланс	- bankBalance
+//18.	Кредитная карта	- creditCardNumber
+//19.	Количество колес у велосипеда	-bikeWheelsQuantity
+//20.	Стоимость билета	- ticketCost
+    }
+}
