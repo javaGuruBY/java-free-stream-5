08561ccecd5297631380d489706e948676384ddb
rus-sto <rus-sto@tut.by>
2020-07-22 09:07:03 +0300

    I did HW2E2

---
+++ homeWork_02/ex2_Types/Ruslan_sto/src/main/java/by/jrr/ruslan_sto/Types.java
@@ -0,0 +1,28 @@
+package by.jrr.ruslan_sto;
+
+public class Types {
+
+    public static void main(String[] args) {
+        System.out.println("//1.Количество кораблей в порту -short, int, byte\n" +
+                "//2.Счетчик целых чисел- long, int, short, byte\n" +
+                "//3.Первое целое число- long, int, short, byte\n" +
+                "//4.Случайное целое число- long, int\n" +
+                "//5.Случайное вещественное число- long, int, short, byte\n" +
+                "//6.Скорость движения автомобиля- int, short, byte\n" +
+                "//7.Введенное пользователем целое число- int, short, byte\n" +
+                "//8.Наибольшее целое число- long, int,\n" +
+                "//9.Наименьшее вещественное число- short, byte\n" +
+                "//10.Левая граница диапазона целых чисел- int, short, byte\n" +
+                "//11.Правая граница диапазона целых чисел - long, int, short\n" +
+                "//12.Загаданное пользователем целое число - int, short, byte\n" +
+                "//13.Денежный баланс - long, int, short\n" +
+                "//14.Количество колес у велосипеда - int, short, byte\n" +
+                "//15.Голодный ли человек - boolean\n" +
+                "//16.Символ латинского алфавита- char\n" +
+                "//17.Четное ли число- boolean\n" +
+                "//18.Текущий год - byte\n" +
+                "//19.Знак пробела- char\n" +
+                "//20.Стоимость билета - int, short, byte, double");
+
+    }
+}
