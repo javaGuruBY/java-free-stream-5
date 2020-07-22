aea8de61593f342a86412dd580dd73fb95cd2248
NikMassV <56344046+NikMassV@users.noreply.github.com>
2020-07-22 19:00:58 +0300

    Homework 3 Task 2

---
+++ homeWork_03/ex2_Human/maistrenko/src/main/java/by/jrr/maistrenko/Human.java
@@ -0,0 +1,16 @@
+package by.jrr.maistrenko;
+
+public class Human {
+
+    private String name;
+    private int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void greet(){
+        System.out.println("Hi! My name is " + name + ". I`m " + age + " years old.");
+    }
+}
---
+++ homeWork_03/ex2_Human/maistrenko/src/main/java/by/jrr/maistrenko/HumanDemo.java
@@ -0,0 +1,8 @@
+package by.jrr.maistrenko;
+
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human john = new Human("John", 20);
+        john.greet();
+    }
+}
