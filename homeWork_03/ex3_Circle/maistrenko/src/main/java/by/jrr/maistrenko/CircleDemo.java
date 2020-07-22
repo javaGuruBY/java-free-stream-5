acf87aa63abe3a183419ff44fd3784997e5eeb69
NikMassV <56344046+NikMassV@users.noreply.github.com>
2020-07-22 19:25:49 +0300

    Homework 3 task 3.

---
+++ homeWork_03/ex3_Circle/maistrenko/src/main/java/by/jrr/maistrenko/CircleDemo.java
@@ -0,0 +1,8 @@
+package by.jrr.maistrenko;
+
+public class CircleDemo {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2);
+        System.out.println(circle.calculateArea());
+    }
+}
