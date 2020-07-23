5509590c00d3ffb5b567f9f0876a4ca8277f693a
rus-sto <rus-sto@tut.by>
2020-07-23 21:11:22 +0300

    my Circle  is Done

---
+++ homeWork_03/ex3_Circle/Ruslan_sto/src/main/java/by/jrr/Ruslan_sto_HW3_Ex3_Circle/Circle.java
@@ -0,0 +1,9 @@
+package by.jrr.Ruslan_sto_HW3_Ex3_Circle;
+
+    public class Circle {
+    public double radius;
+
+    public double calculateArea(){
+    return 3.14 * this.radius * this.radius;
+}
+}
