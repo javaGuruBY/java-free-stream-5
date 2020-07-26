feb5448174909508dbb843d2ea0826d2085ed1ce
“tatianamih” <tmihalchenko66@gmail.com>
2020-07-26 22:38:04 +0300

    My fourth homework is the first task. Defining the number sign.

---
+++ homeWork_04/ex1_SignComparator/tanyamikhalchenko/src/main/java/by/jrr/mikhalchenko/service/App.java
@@ -0,0 +1,9 @@
+package by.jrr.mikhalchenko.service;
+
+import by.jrr.mikhalchenko.bean.SignComparato;
+
+public class App {
+    public static void main(String[] args) {
+        SignComparato signComparato = new SignComparato();
+    }
+}
