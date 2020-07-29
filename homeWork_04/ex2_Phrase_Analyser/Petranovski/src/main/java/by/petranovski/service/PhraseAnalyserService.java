a71407b248980029472954ce014dccc60bef2d6c
VladimirPetranovski <desantluninec@mail.ru>
2020-07-29 16:03:58 +0300

    Petranovski_HW4_Task2_PhraseAnalyser

---
+++ homeWork_04/ex2_Phrase_Analyser/Petranovski/src/main/java/by/petranovski/service/PhraseAnalyserService.java
@@ -0,0 +1,13 @@
+package by.petranovski.service;
+
+public class PhraseAnalyserService {
+    public String analyse(String text) {
+        if (text.startsWith("Make") && text.endsWith("great again")) {
+            return "It stands no chance";
+        } else if (text.startsWith("Make") || text.endsWith("great again")) {
+            return "It could be worse";
+        } else {
+            return "It is fine, really";
+        }
+    }
+}
