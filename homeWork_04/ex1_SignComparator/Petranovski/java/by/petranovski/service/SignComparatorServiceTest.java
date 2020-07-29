4928de186fb3069df485018ad8c3aba318cebe10
VladimirPetranovski <desantluninec@mail.ru>
2020-07-29 12:52:48 +0300

    changes class SignComparatorServiceTest1

--- homeWork_04/ex1_SignComparator/Petranovski/java/by/petranovski/service/SignComparatorServiceTest.java
+++ homeWork_04/ex1_SignComparator/Petranovski/java/by/petranovski/service/SignComparatorServiceTest.java
@@ -10,30 +10,24 @@ public class SignComparatorServiceTest {
     public void comparePositiveTest() {
         SignComparatorService signComparatorService = new SignComparatorService();
         int number = 7;
-        if (7 > 0) {
-            String expected = "Number is positive";
-            String actual = signComparatorService.compare(number);
-            assertEquals(expected, actual);
-        }
+        String expected = "Number is positive";
+        String actual = signComparatorService.compare(number);
+        assertEquals(expected, actual);
     }
     @Test
     public void compareNegativeTest() {
         SignComparatorService signComparatorService = new SignComparatorService();
         int number = - 100;
-        if (-100 > 0) {
-            String expected = "Number is negative";
-            String actual = signComparatorService.compare(number);
-            assertEquals(expected, actual);
-        }
+        String expected = "Number is negative";
+        String actual = signComparatorService.compare(number);
+        assertEquals(expected, actual);
     }
     @Test
     public void compareZeroTest() {
         SignComparatorService signComparatorService = new SignComparatorService();
         int number = 0;
-        if (0 == 0) {
-            String expected = "Number is equal to zero";
-            String actual = signComparatorService.compare(number);
-            assertEquals(expected, actual);
-        }
+        String expected = "Number is equal to zero";
+        String actual = signComparatorService.compare(number);
+        assertEquals(expected, actual);
     }
 }
