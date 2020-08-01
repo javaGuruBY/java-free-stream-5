625782425b6814a0f4e4ae1e60b0a3e121056680
rus-sto <rus-sto@tut.by>
2020-08-01 13:43:29 +0300

    My TelegramBot

---
+++ jis3telegram/jis3telegram/src/test/java/by/jrr/jis3telegram/Jis3telegramApplicationTests.java
@@ -0,0 +1,13 @@
+package by.jrr.jis3telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jis3telegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
